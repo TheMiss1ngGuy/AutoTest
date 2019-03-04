64367e9c2b5e710ebd8120441b20122305096b18
stan <stan@github.com>
2019-03-04 21:03:19 +0800

    httpClient初步使用，并测试获取请求cookies和设置请求cookies

---
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookieseForGet.java
@@ -0,0 +1,7 @@
+package com.course.httpclient.cookies;
+
+/**
+ * Created by Stan on 2017/08/02
+ */
+public class MyCookieseForGet {
+}
---
+++ Chapter9/src/main/java/com/course/httpclient/demo/MyHttpClient.java
@@ -0,0 +1,7 @@
+package com.course.httpclient.demo;
+
+/**
+ * Created by Stan on 2017/08/02
+ */
+public class MyHttpClient {
+}
